ad1110be180cafe8a606c7e9dd080f076decd6ce
Elie Kalouche <116683442+Ekalouche@users.noreply.github.com>
2026-09-10 14:03:40 -0400

    hm

--- LAB1_Perfect_Numbers/LAB1_Perfect_Numbers.cpp
+++ LAB1_Perfect_Numbers/LAB1_Perfect_Numbers.cpp
@@ -11,23 +11,24 @@ int main(int argc, char* argv[])
 {
     long int MaxNumber;
 
+    // Get the upper limit to check, based on command-line arguments
     MaxNumber = GetMaxValue(argc, argv);
 
+    // Loop through every number from 2 up to MaxNumber
     for (long int lcv = 2; lcv <= MaxNumber; lcv++)
     {
+        // Print current number, "\r" returns cursor to start of line
+        // so the next number overwrites it (progress display)
         cout << lcv << "\r";
 
+        // Check if the current number is a perfect number
         if (IsPerfect(lcv))
         {
             cout << lcv << " Is Perfect \n";
         }
-
-        if (IsWeird(lcv))
-        {
-            cout << lcv << " Is Weird \n";
-        }
     }
 
+    // Clear the progress line before exiting
     cout << "         \r";
 
     return 0;
--- LAB1_Perfect_Numbers/module.cpp
+++ LAB1_Perfect_Numbers/module.cpp
@@ -6,25 +6,31 @@
 
 using namespace std;
 
+// Gets the maximum number to check, either from command-line args or user input
 int GetMaxValue(int argc, char* argv[])
 {
     int input; //initalize veriable named input
     if (argc > 1) 
     {
+        // Use the first command-line argument if one was given
         input = atoi(argv[1]);
     }
     else
     {
+        // Otherwise, ask the user to enter a number
         cout << "Please Choose a Number: ";
         cin >> input;
     }
     return input;
 }
 
+// Checks whether a number is "perfect"
+// (a perfect number equals the sum of its proper divisors)
 bool IsPerfect(long int num)
 {
     int sum = 0;
 
+    // Check every number smaller than num to see if it's a divisor
     for (int icv = 1; icv < num; icv++)
     {
         if ((num % icv) == 0)
@@ -33,36 +39,6 @@ bool IsPerfect(long int num)
         }
     }
 
+    // If the sum of divisors equals the number itself, it's perfect
     return sum == num;
-}
-
-
-bool IsWeird(long int num)
-{
-    int sum = 0;
-    int factorT = 0;
-    int factor[100];
-
-    for (int icv = 1; icv < num; icv++)
-    {
-        if ((num % icv) == 0)
-        {
-            factor[icv];
-            factorT++;
-        }
-    }
-
-    for (int icv = 1; icv <= factorT; icv++)
-    {
-       sum = factor[icv];
-    }
-    
-    if (sum > num)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
 }
